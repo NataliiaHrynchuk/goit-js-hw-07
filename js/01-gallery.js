13ac3cef4ea8e72c40c5ec7d46a4e88b383e7b5b
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-05-05 11:39:18 +0300

    changed 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,7 +1,6 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-
 const gallery = document.querySelector('.gallery');
 
 const markup = galleryItems.map(({preview, original, description}) =>
