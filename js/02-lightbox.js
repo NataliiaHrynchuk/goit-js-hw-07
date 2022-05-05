562c4050246b409d8d74a2322c07d3ebbdc51ad4
Nataliia Hrynchuk <natagrinchukk@gmail.com>
2022-05-05 11:40:04 +0300

    added task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,6 +2,26 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
-const listItems = galleryItems.map((galleryItem) =>
-`<li class="li-item" ><img class="image" src = ${galleryItem.url} alt
- = ${galleryItem.description}/></li>`).join("");
+const imageContainer = document.querySelector('.gallery');
+
+const markup = galleryItems.map(({preview, original, description}) =>
+`<a class="gallery__item" href = ${original}><img class="gallery__image" src = ${preview} alt
+ = ${description}/></a>`).join("");
+
+imageContainer.insertAdjacentHTML("afterbegin", markup);
+// imageContainer.addEventListener('click', changeCrsImages);
+
+     
+let gallery = new SimpleLightbox('.gallery a', {
+    overlayOpasity: 0.8,
+    fadeSpeed: 250,
+    captionsData: 'alt',
+    
+});
+  
+gallery.on('show.SimpleLightbox', function (e) {
+    sourceAttr: 'href';
+});
+
+
+
